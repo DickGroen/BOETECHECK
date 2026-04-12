f6bd280f17d450c773641e1cbcf60da697cfb3d9
DickGroen <dickgroen2@gmail.com>
2026-04-12 09:01:34 +0200

    Update footer to display email instead of link
    Replaced contact link with email display in footer.

--- boetecheck/src/app/page.tsx
+++ boetecheck/src/app/page.tsx
@@ -294,7 +294,7 @@ export default function Home() {
             <a href="#faq">FAQ</a>
             <a href="#">Privacybeleid</a>
             <a href="#">Voorwaarden</a>
-            <a href="mailto:support@boetecheck.nl">Contact</a>
+            <span className={styles.footerContact}>support@boetecheck.nl</span>
           </div>
         </div>
         <div className={`container ${styles.footerDisclaimer}`}>
