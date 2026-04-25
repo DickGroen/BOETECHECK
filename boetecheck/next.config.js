8019ece4113e789bb4a8b25b1ffbf9b03c923554
DickGroen <dickgroen2@gmail.com>
2026-04-25 10:46:44 +0200

    Delete boetecheck/next.config.js

--- boetecheck/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['pdf-parse', 'formidable'],
-  },
-}
-
-module.exports = nextConfig
